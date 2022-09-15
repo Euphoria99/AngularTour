31fb5df0d4be575059b00d41fb9edbb20f93c382
PavanBhat <pavan.bhat@thewebpeople.in>
2022-09-15 17:33:46 +0530

    Initial Components  setup

--- src/app/contacts/contacts.module.ts
+++ src/app/contacts/contacts.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ContactsRoutingModule } from './contacts-routing.module';
+import { AddContactComponent } from './add-contact/add-contact.component';
+import { EditContactComponent } from './edit-contact/edit-contact.component';
+import { ListContactComponent } from './list-contact/list-contact.component';
+import { DeleteContactComponent } from './delete-contact/delete-contact.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AddContactComponent,
+    EditContactComponent,
+    ListContactComponent,
+    DeleteContactComponent
+  ],
   imports: [
     CommonModule,
     ContactsRoutingModule
