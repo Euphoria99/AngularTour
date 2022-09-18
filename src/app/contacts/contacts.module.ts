d8b6292ba723732548c7244388539b17765ee5e7
Euphoria99 <363pavanbhat@gmail.com>
2022-09-18 19:19:16 +0530

    Removed unintended imports and changes

--- src/app/contacts/contacts.module.ts
+++ src/app/contacts/contacts.module.ts
@@ -1,23 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { ContactsRoutingModule } from './contacts-routing.module';
 import { AddContactComponent } from './add-contact/add-contact.component';
 import { EditContactComponent } from './edit-contact/edit-contact.component';
 import { ListContactComponent } from './list-contact/list-contact.component';
 import { DeleteContactComponent } from './delete-contact/delete-contact.component';
-
 
 @NgModule({
   declarations: [
     AddContactComponent,
     EditContactComponent,
     ListContactComponent,
-    DeleteContactComponent
+    DeleteContactComponent,
   ],
-  imports: [
-    CommonModule,
-    ContactsRoutingModule
-  ]
+  imports: [CommonModule],
 })
-export class ContactsModule { }
+export class ContactsModule {}
--- src/app/leads/leads.module.ts
+++ src/app/leads/leads.module.ts
@@ -1,14 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { LeadsRoutingModule } from './leads-routing.module';
-
-
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    LeadsRoutingModule
-  ]
+  imports: [CommonModule],
 })
-export class LeadsModule { }
+export class LeadsModule {}
