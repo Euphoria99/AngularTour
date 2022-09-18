fece64664937a54961a5a1bb3e59517c54e2f669
Euphoria99 <363pavanbhat@gmail.com>
2022-09-18 23:35:59 +0530

    Initialized components for profile  module

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { ListProfileComponent } from './list-profile/list-profile.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ListProfileComponent],
+  imports: [CommonModule],
 })
-export class ProfileModule { }
+export class ProfileModule {}
