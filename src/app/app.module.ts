1d75a3dc6e58372c3d2b52d100f71a47cfc85a5f
Euphoria99 <363pavanbhat@gmail.com>
2022-09-19 23:55:02 +0530

    'Custom Directive' example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HighlightDirective],
   imports: [BrowserModule, ProfileModule],
   providers: [],
   bootstrap: [AppComponent],
