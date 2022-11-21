83064bc85441c72756d88ef2d127e0498c6d8e0d
Euphoria99 <363pavanbhat@gmail.com>
2022-11-21 22:51:56 +0530

    Template driven form setup

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-customers',
   templateUrl: './customers.component.html',
-  styleUrls: ['./customers.component.css']
+  styleUrls: ['./customers.component.css'],
 })
 export class CustomersComponent implements OnInit {
+  email: string = '';
+  password: string = '';
+  check: boolean = false;
+  dropdown: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  customer(formValue: NgForm) {
+    console.log('🚀 ~ our form values', formValue.value);
   }
-
 }
